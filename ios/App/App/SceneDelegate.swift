195ab6c606dc60a2f7d0833cc54c7f4f037ef532
Matt Gordon <jobs@mattrgordon.com>
2026-03-10 20:10:53 -0500

    updated files for SceneDelegate

---
+++ ios/App/App/SceneDelegate.swift
@@ -0,0 +1,23 @@
+import UIKit
+import Capacitor
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+
+        let window = UIWindow(windowScene: windowScene)
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateInitialViewController()
+
+        window.rootViewController = vc
+        self.window = window
+        window.makeKeyAndVisible()
+    }
+}
